115621f87f116d81a59204bc2a6dc02b40f58a09
PauliusKu <paulius.kuprevicius@gmail.com>
2018-04-05 16:14:15 +0300

    Beveik baigta

--- main.cpp
+++ main.cpp
@@ -1,10 +1,50 @@
 #include <iostream>
 #include <vector>
+#include <string>
 
 
 
 int main()
 {
-	std::vector <unsigned int> V1;
+	unsigned int n{};
+	
+	std::vector < int> V{};
+	std::vector < int> newV{};
+	std::string Sk{};
+	getline(std::cin, Sk);
+	for (auto& i : Sk)
+	{
+		V.push_back(i-48);
+	}
+	V.push_back(-1);
+	std::cin >> n;
+	
+	for(unsigned int j = 0; j < n; j++)
+	{
+		unsigned int sum = 1;
+		const int s = V.size();
+		for (unsigned int i = 0; i < s-1; i++)
+		{
+			if (V[i] == V[i+1])
+			{
+				sum++;
+			} else if (V[i] != V[i+1])
+			{
+				//std::cout << sum << std::endl;
+				newV.push_back(sum);
+				newV.push_back(V[i]);
+				sum = 1;
+			}
+		}
+		V = newV;
+		V.push_back(-1);
+		newV.clear();
+	}
+	for (unsigned int i = 0; i < V.size()-1; i++)
+	{
+		std::cout << V[i] << " ";
+	}
+	std::cout << std::endl;
+
 	return 0;
 }
